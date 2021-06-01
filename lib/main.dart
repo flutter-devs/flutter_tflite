354a7c82e1604ce10fa60caf7ce4b04efc5e8e9b
mokshbhansali07 <moksh@aeologic.com>
2021-06-01 14:09:36 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
+
+List<CameraDescription> cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  CameraImage cameraImage;
+  CameraController cameraController;
+  String result = "";
+
+  initCamera() {
+    cameraController = CameraController(cameras[0], ResolutionPreset.medium);
+    cameraController.initialize().then((value) {
+      if (!mounted) return;
+      setState(() {
+        cameraController.startImageStream((imageStream) {
+            cameraImage = imageStream;
+            runModel();
+        });
+      });
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: "assets/model.tflite", labels: "assets/labels.txt");
+  }
+
+  runModel() async {
+    if (cameraImage != null) {
+      var recognitions = await Tflite.runModelOnFrame(
+          bytesList: cameraImage.planes.map((plane) {
+            return plane.bytes;
+          }).toList(),
+          imageHeight: cameraImage.height,
+          imageWidth: cameraImage.width,
+          imageMean: 127.5,
+          imageStd: 127.5,
+          rotation: 90,
+          numResults: 2,
+          threshold: 0.1,
+          asynch: true);
+      recognitions.forEach((element) {
+        setState(() {
+          result = element["label"];
+          print(result);
+        });
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initCamera();
+    loadModel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Face Mask Detector"),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Container(
+                height: MediaQuery.of(context).size.height - 170,
+                width: MediaQuery.of(context).size.width,
+                child: !cameraController.value.isInitialized
+                    ? Container()
+                    : AspectRatio(
+                        aspectRatio: cameraController.value.aspectRatio,
+                        child: CameraPreview(cameraController),
+                      ),
+              ),
+            ),
+            Text(
+              result,
+              style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
